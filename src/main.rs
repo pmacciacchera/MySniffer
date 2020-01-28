3a96bbc3e82d49f6d499fb0470c5666f629b84d7
pmacciacchera <44022939+pmacciacchera@users.noreply.github.com>
2020-01-28 14:33:19 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crate::lib::ParserPaolo::ParserPaolo;
 use rtsparse::*;
     
 fn main() {
-<<<<<<< HEAD
+
   let mut cap = Device::lookup().unwrap().open().unwrap();
   while let Ok(packet) = cap.next() {
        ParserPaolo(packet.data);
@@ -13,6 +13,4 @@ fn main() {
   
            
 }
-=======
-}
->>>>>>> 152c21017a1006dcf03110597f50fb1236e8822f
+
