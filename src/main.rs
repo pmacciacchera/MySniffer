9d0166f225ef4229b87b3f5a1e9113ff69054e88
pmacciacchera <polmacra@outlook.it>
2020-01-28 14:03:05 +0100

    modifiche in data 28 gennaio con una versione più funzionale del parser e ampliamento dei protocolli riconosciuti

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod lib;
+
+use pcap::Device;
+use crate::lib::ParserPaolo::ParserPaolo;
+use rtsparse::*;
+    
 fn main() {
-    println!("Hello, world!");
+  let mut cap = Device::lookup().unwrap().open().unwrap();
+  while let Ok(packet) = cap.next() {
+       ParserPaolo(packet.data);
+  }
+  
+           
 }
