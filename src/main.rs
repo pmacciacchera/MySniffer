758d60d1e70d2e3bba6ae15894aa7924b497a92f
pmacciacchera <polmacra@outlook.it>
2020-02-05 16:01:01 +0100

    change 5 feb

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod lib;
 
 use pcap::Device;
-use crate::lib::ParserPaolo::ParserPaolo;
-use rtsparse::*;
+use crate::lib::parser_ethernet::parser_ethernet;
+
     
 fn main() {
-
   let mut cap = Device::lookup().unwrap().open().unwrap();
   while let Ok(packet) = cap.next() {
-       ParserPaolo(packet.data);
+       parser_ethernet(packet.data);
   }
   
            
 }
-
