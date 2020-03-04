63fd3992bdfb523d96e9b625c173f1f3159c6290
Alessandro Recchia <recchiaalessandro0@gmail.com>
2020-03-04 13:09:41 +0100

    added capture with parameters, streaming capture and capture to and from a file.

--- src/lib/packet_capture.rs
+++ src/lib/packet_capture.rs
@@ -1,43 +1,54 @@
-extern crate pcap;
-use pcap::{Device, Capture, Active};
+/*
+* This file implement the packet capture in different variants *to complete*
+*
+* Author "Alessandro Recchia <recchiaalessandro0@gmail.com>
+*
+*/
 
-pub struct  InitializeCapture {
+extern crate pcap;
+
+use pcap::{Active, Capture, Device};
+
+/*
+* Initialize the capture with some useful parameters
+*/
+pub struct  CaptureBuilder {
     promisc_parameter: bool,
     timeout_parameter: i32,
     buffer_size_parameter: i32,
     snaplen_parameter: i32,
 }
 
-impl InitializeCapture  {
-    pub fn new() -> InitializeCapture {
-        InitializeCapture {
+impl CaptureBuilder  {
+    // new() initialize a new Capture with default parameters
+    pub fn new() -> CaptureBuilder {
+        CaptureBuilder {
             promisc_parameter : false,
             timeout_parameter : 0,
             buffer_size_parameter : 1000000,
             snaplen_parameter: 65535,
         }
     }
-
-    pub fn set_promisc (&mut self, value: bool) -> &mut InitializeCapture {
+    pub fn set_promisc (&mut self, value: bool) -> &mut CaptureBuilder {
         self.promisc_parameter=value;
         self
     }
 
-    pub fn set_timeout(&mut self, value :i32) -> &mut InitializeCapture {
+    pub fn set_timeout(&mut self, value :i32) -> &mut CaptureBuilder {
         self.timeout_parameter = value;
         self
     }
 
-    pub fn set_buffer_size (&mut self, value : i32) -> &mut InitializeCapture {
+    pub fn set_buffer_size (&mut self, value : i32) -> &mut CaptureBuilder {
         self.buffer_size_parameter=value;
         self
     }
 
-    pub fn set_snaplen (&mut self, value : i32) -> &mut InitializeCapture {
+    pub fn set_snaplen (&mut self, value : i32) -> &mut CaptureBuilder {
         self.snaplen_parameter= value;
         self
     }
-
+// finalize() create the StartCapture useful to start the packet capture with parameters
     pub fn finalize(&self) -> StartCapture {
         StartCapture {
             promisc_parameter: self.promisc_parameter,
@@ -60,7 +71,7 @@ impl StartCapture {
         self.promisc_parameter
     }
 
-    pub fn get_timout_parameter(&self) -> i32 {
+    pub fn get_timeout_parameter(&self) -> i32 {
         self.timeout_parameter
     }
 
@@ -68,20 +79,20 @@ impl StartCapture {
         self.buffer_size_parameter
     }
 
-    pub fn get_snaplen(&self) -> i32 {
+    pub fn get_snaplen_parameter(&self) -> i32 {
         self.snaplen_parameter
     }
 
     fn active_capture(initialized_capture: StartCapture, device: Device) -> Capture<Active> {
         let mut start_capture = Capture::from_device(device).unwrap()
-            .promisc(initialized_capture.promisc_parameter)
-            .snaplen(initialized_capture.snaplen_parameter)
-            .timeout(initialized_capture.timeout_parameter)
-            .buffer_size(initialized_capture.buffer_size_parameter)
+            .promisc(initialized_capture.get_promisc_parameter())
+            .snaplen(initialized_capture.get_snaplen_parameter())
+            .timeout(initialized_capture.get_timeout_parameter())
+            .buffer_size(initialized_capture.get_buffer_size_parameter())
             .open().unwrap();
         return start_capture
     }
-
+// capture live in streaming without saving packet to file
     pub fn streaming_capture(mut initialized_capture: StartCapture, device: Device) {
         let mut start_capture = StartCapture::active_capture(initialized_capture, device);
         while let Ok(packet) = start_capture.next() {
